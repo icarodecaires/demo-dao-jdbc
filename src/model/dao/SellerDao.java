2fd6654f9c19774a81b88abd0eee882d8015bcff
Icaro Caires <icaro_de_caires@outlook.com>
2021-02-22 20:19:55 -0400

    Implementando busca por departamento

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,7 +2,7 @@ package model.dao;
 
 import java.util.List;
 
-
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -16,4 +16,6 @@ public interface SellerDao {
 	Seller findById(Integer id);
 	
 	List<Seller> findAll();
+	
+	List<Seller> findByDepartment(Department department);
 }
