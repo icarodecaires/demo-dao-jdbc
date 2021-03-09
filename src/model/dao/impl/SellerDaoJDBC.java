87b6a35346b8524d02f4d8f19dbcb78045e1d1ea
Icaro Caires <icaro_de_caires@outlook.com>
2021-03-08 22:00:47 -0400

    Pareando

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -36,7 +36,7 @@ public class SellerDaoJDBC implements SellerDao {
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
-			st.setDouble(4, obj.getBaseSalry());
+			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
 
 			int rowsAffectd = st.executeUpdate();
@@ -71,7 +71,7 @@ public class SellerDaoJDBC implements SellerDao {
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
-			st.setDouble(4, obj.getBaseSalry());
+			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
 			st.setInt(6, obj.getId());
 
